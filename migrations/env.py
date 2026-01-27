8d05ead3fe0f844508a47b065bc27bb9a154f41a
Force-in-Motion <canzdao@gmail.com>
2026-01-27 15:13:31 +0300

    refactor(migrations): alembic env

--- migrations/env.py
+++ migrations/env.py
@@ -6,7 +6,7 @@ from sqlalchemy import pool
 from sqlalchemy.engine import Connection
 from sqlalchemy.ext.asyncio import async_engine_from_config
 
-from app.models import Base
+from app.database.models import Base
 from app.core.config import db_settings
 
 # this is the Alembic Config object, which provides
