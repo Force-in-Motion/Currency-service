efaaca79dec33a7d09c943ff63400a024f9e12d0
Force-in-Motion <canzdao@gmail.com>
2026-01-27 14:12:38 +0300

    feat(tools): add custom exceptions

--- app/tools/exeptions.py
+++ app/tools/exeptions.py
@@ -25,46 +25,6 @@ class HTTPErrors(Exception):
         detail="Bad request",
     )
 
-    unauthorized = HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="Invalid login or password",
-    )
-
-    user_inactive = HTTPException(
-        status_code=status.HTTP_403_FORBIDDEN,
-        detail="User inactive",
-    )
-
-    token_invalid = HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="Invalid token",
-    )
-
-    cart_empty = HTTPException(
-        status_code=status.HTTP_404_NOT_FOUND,
-        detail="Cart empty or not found",
-    )
-
-    not_admin = HTTPException(
-        status_code=status.HTTP_403_FORBIDDEN,
-        detail="User not admin",
-    )
-
-    err_update_model = HTTPException(
-        status_code=status.HTTP_400_BAD_REQUEST,
-        detail="Error update model in DB",
-    )
-
-    err_create_model = HTTPException(
-        status_code=status.HTTP_400_BAD_REQUEST,
-        detail="Error create model in DB",
-    )
-
-    err_delete_model = HTTPException(
-        status_code=status.HTTP_400_BAD_REQUEST,
-        detail="Error delete model in DB",
-    )
-
     clear_table = HTTPException(
         status_code=status.HTTP_400_BAD_REQUEST,
         detail="Error cleared table",
