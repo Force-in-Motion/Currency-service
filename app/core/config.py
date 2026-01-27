d08d33632bfe09af169c185d2cd9074ac7ac4812
Force-in-Motion <canzdao@gmail.com>
2026-01-27 11:58:00 +0300

    feat(app): add settings in config by .env file

---
+++ app/core/config.py
@@ -0,0 +1,19 @@
+from pydantic import ConfigDict
+from pydantic_settings import BaseSettings
+
+
+
+class DBSettings(BaseSettings):
+    """ Определяет настройки баз данных, которые считываются из .env файла """
+    url: str
+
+    echo: bool
+
+    celery_broker_url: str
+
+    celery_backend_url: str
+
+    model_config = ConfigDict(
+        extra="ignore",
+        env_prefix="DB_",
+    )
