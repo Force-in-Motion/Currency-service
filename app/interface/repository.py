4b2c698a0db516ee101a32980454c5aad8279a92
Force-in-Motion <canzdao@gmail.com>
2026-01-27 13:00:07 +0300

    feat(interface): add interface for repositories

---
+++ app/interface/repository.py
@@ -0,0 +1,46 @@
+from typing import Optional
+from abc import ABC, abstractmethod
+from app.tools.types import DBModel
+
+
+class ARepo(ABC):
+
+    @classmethod
+    @abstractmethod
+    async def get_all(cls, *args, **kwargs) -> Optional[list[DBModel]]:
+        """
+        Возвращает все модели из БД
+        :return: Список всех моделей | None
+        """
+        pass
+
+
+
+    @classmethod
+    @abstractmethod
+    async def get_last(cls, *args, **kwargs) -> Optional[DBModel]:
+        """
+        Возвращает последнюю добавленную модель из БД
+        :return: Модель | None
+        """
+        pass
+
+
+    @classmethod
+    @abstractmethod
+    async def get_by_date(cls, *args, **kwargs) -> Optional[list[DBModel]]:
+        """
+        Возвращает список всех моделей, добавленных за указанный интервал времени
+        :return: Список моделей | None
+        """
+        pass
+
+
+    @classmethod
+    @abstractmethod
+    async def clear(cls, *args, **kwargs) -> list:
+        """
+        Очищает БД моделей определенной категории и сбрасывает последовательность id моделей
+        :return: Пустой список
+        """
+        pass
